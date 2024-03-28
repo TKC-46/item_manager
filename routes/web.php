44e2ff2228ed6b99c9a12350793a2d4f8b108849
TKC-46 <155297764+TKC-46@users.noreply.github.com>
2024-03-28 23:46:02 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,12 @@ Route::get('/', function () {
 });
 
 // 商品一覧の表示
-// get('/item', ItemController@index)
 Route::get('/item', [ItemController::class, 'index'])->name("item.index");
 // 商品登録ページ
 Route::get('/item/add', [ItemController::class, 'showAdd'])->name("item.showAdd");
 // 商品登録の実行
 Route::post('/item/add', [ItemController::class, 'add'])->name("item.add");
 // 商品編集ページ
-// {item}はワイルドカードこのルートのパスは /item/ から始まり、その後に任意の値が続く形になります。
-// 具体的な商品のIDや識別子などが入ることが想定されている
 Route::get('/item/{item}', [ItemController::class, 'showEdit'])->name("item.showEdit");
 // 更新処理
 Route::post('/item/{item}/edit', [ItemController::class, 'edit'])->name("item.edit");
@@ -37,8 +34,3 @@ Route::post('/item/{item}/edit', [ItemController::class, 'edit'])->name("item.ed
 Route::post('/item/{item}', [ItemController::class, 'delete'])->name("item.delete");
 
 
-
-Route::get('/admin', [AdminController::class, 'index']);
-Route::get('/admin/edit/{id}', [AdminController::class, "showEdit"]);
-Route::get("/admin/add", [AdminController::class, "showAdd"]);
-Route::post("/admin/add", [AdminController::class, "add"]);
